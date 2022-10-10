0a514c1d1012cb45a0d3e88e01dbdd013449b8a9
sarathanatarajan <sarathanatarajan17@gmail.com>
2022-10-10 07:26:43 +0000

    Upload New File

---
+++ RectanglePerimeter.java
@@ -0,0 +1,28 @@
+package com.stackroute.basics;
+
+import java.util.Scanner;
+
+public class RectanglePerimeter {
+    public static void main(String[] args) {
+        new RectanglePerimeter().getValues();
+    }
+
+    //get user input from console
+    public void getValues() {
+    	Scanner sc = new Scanner(System.in);
+    	System.out.println(" Enter  the length & breadth : ");
+    	double length = sc.nextDouble();
+    	double breadth = sc.nextDouble();
+    	double perimeter =new RectanglePerimeter().perimeterCalculator(length, breadth);
+    	System.out.println(" Perimeter of the Rectangle :: "+perimeter);
+    	
+
+    }
+
+    //write logic to find perimeter of rectangle here
+    public double perimeterCalculator(double length, double breadth) {
+    	//your business logic 
+    	double perimeter = 2 * (length + breadth);
+        return perimeter;
+    }
+}
