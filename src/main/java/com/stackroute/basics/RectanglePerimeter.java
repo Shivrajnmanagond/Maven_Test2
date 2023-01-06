b25a2ba5b275930f57cc2b61a0df6dae4ce1d3b2
SarathaNatraj <102175569+SarathaNatraj@users.noreply.github.com>
2023-01-06 19:15:33 +0530

    Updated code

--- src/main/java/com/stackroute/basics/RectanglePerimeter.java
+++ src/main/java/com/stackroute/basics/RectanglePerimeter.java
@@ -1,6 +1,8 @@
 package com.stackroute.basics;
 
-
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RectanglePerimeter {
     public static void main(String[] args) {
@@ -9,11 +11,18 @@ public class RectanglePerimeter {
 
     //get user input from console
     public void getValues() {
-
-    }
+    	Scanner sc = new Scanner(System.in);
+    	System.out.println(" Enter  the length & breadth : ");
+    	double length = sc.nextDouble();
+    	double breadth = sc.nextDouble();
+    	double perimeter =new RectanglePerimeter().perimeterCalculator(length, breadth);
+    	System.out.println(" Perimeter of the Rectangle :: "+perimeter);
+     }
 
     //write logic to find perimeter of rectangle here
     public double perimeterCalculator(double length, double breadth) {
-        return 0;
+    	//your business logic 
+    	double perimeter = 2 * (length + breadth);
+        return perimeter;
     }
 }
